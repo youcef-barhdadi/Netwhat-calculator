a88ed28427a0d00caa3eb4c29361e5e9e72ed1b5
Youcef Barhdadi <ybarhdad@e2r13p5.1337.ma>
2019-11-02 02:06:44 +0100

    a

--- srcs/nt_ntw_adr.c
+++ srcs/nt_ntw_adr.c
@@ -6,7 +6,7 @@
 /*   By: lacollar <lacollar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 08:39:33 by lacollar          #+#    #+#             */
-/*   Updated: 2019/10/31 14:21:45 by lacollar         ###   ########.fr       */
+/*   Updated: 2019/11/02 02:06:03 by ybarhdad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,87 +15,139 @@
 #include "ft_substr.c"
 #include "nt_strrchr_modified.c"
 #include <string.h>
-
-char *getsubmask(int cid)
+#include <math.h>
+char class(char *nbr)
 {
-	if (cid == 1)
-		return "128.0.0.0";
-	if (cid == 2)
-		return "192.0.0.0";
-	if (cid == 3)
-		return "224.0.0.0";
-	if (cid == 4)
-		return "240.0.0.0";
-	if (cid == 5)
-		return "248.0.0.0";
-	if (cid == 6)
-		return "252.0.0.0";
-	if (cid == 7)
-		return "254.0.0.0";
-	if (cid == 8)
-		return "255.0.0.0";
-	if (cid == 9)
-		return "225.128.0.0";
-	if (cid == 10)
-		return "255.192.0.0";
-	if (cid == 11)
-		return "255.224.0.0";
-	if (cid == 12)
-		return "255.240.0.0";
-	if (cid == 13)
-		return "255.248.0.0";
-	if (cid == 14)
-		return "255.252.0.0";
-	if (cid == 15)
-		return "255.254.0.0";
-	if (cid == 16)
-		return "255.255.0.0";
-	if (cid == 17)
-		return "255.255.128.0";
-	if (cid == 18)
-		return "255.255.192.0";
-	if (cid == 19)
-		return "255.255.224.0";
-	if (cid == 20)
-		return "255.255.240.0";
-	if (cid == 21)
-		return "255.255.248.0";
-	if (cid == 22)
-		return "255.255.252.0";
-	if (cid == 23)
-		return "255.255.254.0";
-	if (cid == 24)
-		return "255.255.255.0";
-	if (cid == 25)
-		return "225.255.255.128";
-	if (cid == 26)
-		return "255.255.255.192";
-	if (cid == 27)
-		return "255.255.255.224";
-	if (cid == 28)
-		return "255.255.255.240";
-	if (cid == 29)
-		return "255.255.255.248";
-	if (cid == 30)
-		return "255.255.255.252";
-	if (cid == 31)
-		return "255.255.255.254";
-	if (cid == 32)
-		return "255.255.255.255";
-	return NULL;
+	int a = atoi(nbr);
+
+	if (a >= 0 && a <= 127)
+		return 'A';
+	else if (a >= 128 && a <191 )
+		return 'B';
+	else if (a >=192 && a <= 223)
+		return 'C';
+	else if (a >= 224 && a <= 239)
+		return 'D';
+	else 
+		return 'E';
+}
+
+
+
+
+
+
+int	*to_bin(char **str)
+{
+	int *re = malloc(sizeof(int) * 32);	
+	int a;
+	int b;
+	int c;
+	int d;
+	int i = 0;
+
+	if (!str)
+		return 0;
+	a = atoi(str[0]);
+	b = atoi(str[1]);
+	c = atoi(str[2]);
+	d = atoi(str[3]);
+
+	i = 8;
+	while (i-- > 0)
+	{
+		re[i] = a % 2;
+		a /= 2;
+	}
+	i = 16;
+	while (i-- > 8)
+	{
+		re[i] = b % 2;
+		b /= 2;
+	}
+	i = 24;
+	while (i-- > 16)
+	{
+		re[i] = c % 2;
+		c /= 2;
+	}
+	i = 32;
+	while (i-- > 24)
+	{
+		re[i] = d % 2;
+		d /= 2;
+	}
+	return re;
+
+}
+
+int *to_dec(int *bin)
+{
+	int *re = malloc(sizeof(int) * 4);
+	int i;
+	int j;
+	
+
+
+	if (!bin)
+		return 0;
+		i = 0;
+	j = 0;
+	bzero(re, sizeof(int) * 4);
+	int k  = 7;
+	while (i < 8)
+	{
+		re[j] = pow(2, k) *  bin[i]  + re[j] ;
+		k--;
+		i++;
+	}
+	j++;
+	k= 7;
+	while (i < 16)
+	{	
+		re[j] = pow(2, k) *  bin[i]  + re[j] ;
+		i++;
+		k--;
+	}
+	j++;
+	k = 7;
+	while (i < 24)
+	{	
+		re[j] = pow(2, k) *  bin[i]  + re[j] ;
+		k--;
+		i++;
+	}
+	j++;
+	k  = 7;
+	while (i < 32)
+	{	
+		re[j] = pow(2, k) *  bin[i]  + re[j] ;
+		i++;
+		k--;
+	}
+	return re;
+
 }
 
 void nt_ntw_adr(char *address)
 {
 	char	**octet;
-	char	**mask;
 	char	*suffix;
 	char	*ip;
 	int		CIDR;
 	int		i;
 	int		len;
 	int		a;
-	
+	int		*bor;
+	int		*net_bin;
+	int		*bor_bin;
+	int		*network_addr;
+	int		*broadcast_addr;
+	int		*adr;
+	net_bin = malloc(sizeof(int) * 32);
+	bor_bin  = malloc(sizeof(int) * 32);
+
+
 	if (!(suffix = malloc(sizeof(char) * 3)))
 		return ; 
 	len =   suffix - address - 1;
@@ -105,34 +157,52 @@ void nt_ntw_adr(char *address)
 		return ;
 	octet = ft_split(ip, '.');
 	CIDR = atoi(suffix);
-	mask =  ft_split(getsubmask(CIDR), '.');
 	i = 0;
-	printf("network address\t\t");
+	int z = 32 - CIDR;
+	adr = to_bin(octet);
+	for (i = 0; i <= 31 - (31 - z); i++)
+	{
+		bor_bin[i] = adr[i];
+		net_bin[i] = adr[i];
+	}
+	for (i = 31 ; i > (31- CIDR);i--)
+	{
+		bor_bin[i] = 1;
+		net_bin[i] = 0;
+	}
+	
+	network_addr = to_dec(net_bin);  
+	broadcast_addr = to_dec(bor_bin);
+
+
+	printf("boradcast : ");
+	i = 0;
 	while (i < 4)
 	{
-		a = atoi(octet[i]) & atoi(mask[i]);
-		printf("%d%s",a,(i != 3? ".":""));
+		printf("%d%s",broadcast_addr[i],(i != 3)?".":"");
 		i++;
 	}
-	printf("\nbroadcast address\t");
 	i = 0;
+	printf("Network Adress :");
 	while (i < 4)
-	{	
-		int oc  = atoi(mask[i]);	
-		if (oc !=  255)
-			printf("255%s",(i != 3? ".":""));
-		else 
-			printf("%s%s",octet[i],(i != 3? ".":""));
+	{
+		printf("%d%s",network_addr[i],(i != 3)?".":"");
 		i++;
 	}
+	
+
 }
 
 int 	main(int argc, char **argv)
 {
+
+
+
     if (argc == 2)
     {
 		nt_ntw_adr(argv[1]);
 		return (0);
     }
+	
 	return (0);
 }
